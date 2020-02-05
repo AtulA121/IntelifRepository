6f4bb22851fb7cfb6971334940f7f2c5ee23a93e
kiranpwr1260 <kiranpwr143@gmail.com>
2020-02-05 10:00:22 +0530

    ReverseString program.

---
+++ src/ReverseString.java
@@ -0,0 +1,11 @@
+public class ReverseString {
+    public static void main(String[] args){
+        String str="atul pisal";
+        StringBuilder newStr= new StringBuilder();
+        int temp=str.length()-1;
+        for(int i=temp;i>=0;i--){
+            newStr.append(str.charAt(i));
+        }
+        System.out.println("Done : "+newStr);
+    }
+}
